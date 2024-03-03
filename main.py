5f96cfaa48e32c1735875493ecb35281d6ecd091
Sina <s.keyhaninejad@gmail.com>
2024-03-03 23:48:02 +0000

    the first version

---
+++ main.py
@@ -0,0 +1,8 @@
+#Input command used to allow user to insert the following information 
+
+Name = input("Enter name here ")
+Age = input("Enter age here ")
+House_Number = input("Enter House Number here ")
+Stree_Name = input("Enter Street Name here ")
+
+print(f'This is {Name}. {Name} is {Age} years old and lives at house number {House_Number} on {Stree_Name}')
